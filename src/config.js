60d5c35467320f28ee0969b190262cc2454ca2df
Gustavo OON <95374864+GustavoOON@users.noreply.github.com>
2022-06-07 11:26:40 -0300

    up

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const prod = "http://dashboardbff.oonpayperuse.com.br";
 const dev = "http://localhost:8080";
 
 const tokenMaps = "AIzaSyB9I7snvZRRQ6faupULeBXYz5tU2AUuWek"; // TOKEN PESSOAL
-const urlDomain = prod;
+const urlDomain = dev;
 
 export const configCookies = () => {
     const token = Cookies.get("TokenID");
